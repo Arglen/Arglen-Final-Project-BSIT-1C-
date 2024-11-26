afcc18979a97ac9378c658e7bffa1f1de55a9ac2
Arglen <arglenpogi@gmail.com>
2024-11-26 22:09:42 +0800

    Updated

--- Final.py
+++ Final.py
@@ -1,1 +1,9 @@
-print("finall")
+
+def part1():
+    print(""" \n=============| ARGLEN L. ELUMBA |============\n============| COMPILATION PROJECT |============ """)
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    user = str(input("Please Choose a number: "))
+    pass
+part1()

77d134e1c2300b402ba41fd0d593fd2f558f20d0
Arglen <arglenpogi@gmail.com>
2024-11-26 22:34:04 +0800

    Updated

--- Final.py
+++ Final.py
@@ -1,9 +1,29 @@
+import os
+def code_challenge():
+    pass
+
+def activities():
+    pass
+
+def part0():
+    print("""========= WELCOME TO MY COMPILATION PROJECT ========= \n""")
+    welcome = str(input("\t     TYPE (YES) TO CONTINUE: "))
+    if welcome == "yes":
+        print("\n")
+    os.system('cls')
 
 def part1():
-    print(""" \n=============| ARGLEN L. ELUMBA |============\n============| COMPILATION PROJECT |============ """)
-    print("1. ")
-    print("2. ")
-    print("3. ")
+    print(""" \t\n=============| ARGLEN L. ELUMBA |============\t\n============| COMPILATION PROJECT |============ \n""")
+    print("1. Code Challenges")
+    print("2. Acivities \n")
+    print("")
     user = str(input("Please Choose a number: "))
-    pass
+
+    if user == "1":
+        
+
+    elif user == "2":
+        pass
+
+part0()
 part1()

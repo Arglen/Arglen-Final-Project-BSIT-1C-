d2eb8899a03f9bacea3103204edf7247779ff79d
Arglen <Arglenpogi1@gmail.com>
2024-12-05 20:50:59 +0800

    Updated

---
+++ Final1.py/Act_Modules/act_modules1.py
@@ -0,0 +1,59 @@
+import os
+
+def activity_intro():
+    while True:
+        print("====== ACTIVITIES ====== \n")
+        print("\t1. Activity \t\t 2. Activity ")
+        print("\t3. Activity \t\t 4. Activity ")
+        print("\t5. Activity \t\t 6. Activity ")
+        print("\t7. Activity \t\t 8. Activity ")
+        print("\t9. Activity \t\t 10. Activity ")
+        print("\t11. Activity \t\t 12. Activity ")
+        print("\t13. Activity \t\t 14. Activity ")
+        print("\t15. Activity \t\t 16. Activity ")
+        print("\t17. Activity \t\t 18 Activity ")
+        print("\t19. Activity \t\t 20. Activity ")
+        print("\t30. Exit\n")
+        user = str(input("ENTER A NUMBER: "))
+        print("=========================\n")
+
+        os.system('cls')
+
+        if user == "1":
+            pass
+        elif user == "2":
+            pass
+        elif user == "3":
+            pass
+        elif user == "4":
+            pass
+        elif user == "5":
+            pass
+        elif user == "6":
+            pass
+        elif user == "7":
+            pass
+        elif user == "8":
+            pass
+        elif user == "9":
+            pass
+        elif user == "10":
+            pass
+        elif user == "11":
+            pass
+        elif user == "12":
+            pass
+        elif user == "13":
+            pass
+        elif user == "14":
+            pass
+        elif user == "15":
+            pass
+        elif user == "16":
+            pass
+        elif user == "0":  
+            print("Return...")
+            return
+        else:
+            print("Invalid option. Please try again.")
+            print("=========================\n")
